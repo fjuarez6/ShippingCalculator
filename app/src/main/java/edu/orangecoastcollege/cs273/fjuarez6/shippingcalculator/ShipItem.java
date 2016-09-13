0b5aa2c7e3ad5b5498030b9f327bef06bca559b5
freddy <frddyjrz@gmail.com>
2016-09-12 22:36:48 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/fjuarez6/shippingcalculator/ShipItem.java
@@ -0,0 +1,41 @@
+package edu.orangecoastcollege.cs273.fjuarez6.shippingcalculator;
+
+/**
+ * Created by frddy on 9/12/2016.
+ */
+public class ShipItem {
+
+    private double mWeight;
+    private double mBaseCost;
+    private double mAddedCost;
+    private double mTotalCoast;
+
+    public ShipItem() {
+        mWeight = 0.0;
+        mBaseCost = 3.00;
+        mAddedCost = 0.0;
+        mTotalCoast = 0.0;
+    }
+
+    public double getWeight() {
+        return mWeight;
+    }
+
+    public void setWeight(double weight) {
+        mWeight = weight;
+        mAddedCost = ((weight - 16) / 4) * (.50);
+        mTotalCoast = mBaseCost + mAddedCost;
+    }
+
+    public double getBaseCost() {
+        return mBaseCost;
+    }
+
+    public double getAddedCost() {
+        return mAddedCost;
+    }
+
+    public double getTotalCoast() {
+        return mTotalCoast;
+    }
+}
